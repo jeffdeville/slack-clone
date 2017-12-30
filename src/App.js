11a96f0cb18283e79b7622effee897709b87e20a
Jeff Deville <jeffdeville@gmail.com>
2017-12-30 06:38:30 -0500

    Remove redux, and scaffold in AppSync - channels loading

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import Navbar from './components/navbar'
-import ChannelList from './containers/channel_list'
-import ChannelDetail from './containers/channel_detail'
-import { Provider } from 'react-redux'
-import { createStore } from "redux";
-import reducers from './reducers'
+
+import appSyncConfig from './AppSync'
+import { ApolloProvider } from 'react-apollo'
+import AWSAppSyncClient from 'aws-appsync'
+import { Rehydrated } from 'aws-appsync-react'
+
 import './App.css';
+import logo from './logo.svg';
+import Navbar from './Components/Navbar'
+import ChannelList from './Components/ChannelList'
+import ChannelDetail from './Components/ChannelDetail'
+
+const client = new AWSAppSyncClient({
+  url: appSyncConfig.graphqlEndpoint,
+  region: appSyncConfig.region,
+  auth: {
+    type: appSyncConfig.authenticationType,
+    apiKey: appSyncConfig.apiKey
+  }
+})
+
+console.log(appSyncConfig)
+console.log(client)
 
 class App extends Component {
   render() {
     return (
-      <Provider store={createStore(reducers)}>
-        <div className="App full-screen">
-          <Navbar logo={logo} />
-          <div className="main">
-            <ChannelList />
-            <ChannelDetail />
+      <ApolloProvider client={client}>
+        <Rehydrated>
+          <div className="App full-screen">
+            <Navbar logo={logo} />
+            <div className="main">
+              <ChannelList />
+              <ChannelDetail />
+            </div>
           </div>
-        </div>
-      </Provider>
+        </Rehydrated>
+      </ApolloProvider>
     );
   }
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './navbar.css'
+import './Navbar.css'
 
 const Navbar = (props) => {
   return (
