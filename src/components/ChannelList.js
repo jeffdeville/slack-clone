e92a3511391ed40cefa75a87db22df0f0470075e
Jeff Deville <jeffdeville@gmail.com>
2018-01-02 12:04:34 -0500

    Enabled subscriptions for creating new channels

--- src/components/ChannelList.js
+++ src/components/ChannelList.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react'
 import './ChannelList.css'
-import AllChannels from '../Queries/AllChannels'
+import AllChannels from '../GraphQL/AllChannels'
+import SubscriptionNewChannel from '../GraphQL/SubscriptionNewChannel'
 import { graphql } from 'react-apollo'
 import { Link } from "react-router-dom";
 
 import CreateChannel from './CreateChannel'
 
 class ChannelList extends Component {
+  componentWillReceiveProps(nextProps) {
+    if (!nextProps.data.loading) {
+      if (this.unsubscribe) { return }
+    }
+    this.unsubscribe = nextProps.data.subscribeToMore({
+      document: SubscriptionNewChannel,
+      updateQuery: (prev, { subscriptionData: { data: { putChannel } } }) => {
+        return Object.assign({}, prev, {
+          allChannels: [
+            ...prev.allChannels.filter(({id}) => {
+              return id !== putChannel.id
+            }),
+            putChannel
+          ]
+        })
+      }
+    })
+  }
+
   renderList() {
     return this.props.data.allChannels.map((channel) => {
       return (
